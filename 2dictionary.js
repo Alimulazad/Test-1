8f3432f514716b4b8597a37e84fef600ed077199
Alimulazad <alimulazad5@gmail.com>
2025-08-01 23:35:49 +0600

    Update and rename style.css to 2dictionary.js

---
+++ 2dictionary.js
@@ -0,0 +1,11 @@
+body {
+  font-family: 'Noto Sans Bengali', sans-serif;
+  background: #f9fafb;
+  color: #111827;
+  max-width: 600px;
+  m
+
+.form-box {
+  background: #ffffff;
+  padding: 20px;
+  borde
